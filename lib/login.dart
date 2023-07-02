79753595ce441a4841efb626f828ab0bfeebda15
Frank Willian <79548949+FranckWill@users.noreply.github.com>
2023-07-01 22:12:58 -0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -16,8 +16,12 @@ class TelaLogin extends StatefulWidget {
 
 class _TelaLoginState extends State<TelaLogin> {
 
-  final TextEditingController controllerLogin = TextEditingController();
-  final TextEditingController controllerSenha = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _controllerAvatar = TextEditingController();
+  final TextEditingController _controllerNome = TextEditingController();
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerLogin = TextEditingController();
+  final TextEditingController _controllerSenha = TextEditingController();
 
   void autenticar(context, login, senha) async{
     Usuario? usr = await widget.bd!.autenticacao(login, senha);
@@ -45,6 +49,119 @@ class _TelaLoginState extends State<TelaLogin> {
     }
   }
 
+  void adicionarNovoUsuario(){
+    showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: const Text("Adicionar usuário"),
+            content: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _controllerAvatar,
+                          keyboardType: TextInputType.url,
+                          decoration: const InputDecoration(
+                              hintText: "Insira uma url",
+                              labelText: "Avatar"
+                          ),
+                          validator: (url){
+                            if(url == null || url.isEmpty){
+                              return "digite uma url";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _controllerNome,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                              hintText: "Insira um nome",
+                              labelText: "Nome"
+                          ),
+                          validator: (nome){
+                            if(nome == null || nome.isEmpty){
+                              return "Insira um nome";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _controllerEmail,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                              hintText: "Insira um e-mail",
+                              labelText: "E-mail"
+                          ),
+                          validator: (email){
+                            if(email == null || email.isEmpty){
+                              return "Insira um e-mail";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _controllerLogin,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                              hintText: "Insira um login",
+                              labelText: "Login"
+                          ),
+                          validator: (login){
+                            if(login == null || login.isEmpty){
+                              return "Insira um login";
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _controllerSenha,
+                          obscureText: true,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                              hintText: "Insira uma senha",
+                              labelText: "Senha"
+                          ),
+                          validator: (senha){
+                            if(senha == null || senha.isEmpty){
+                              return "Insira uma senha";
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                  onPressed: () async{
+                    if(_formKey.currentState!.validate()){
+                      await widget.bd!.inserirUsuario(Usuario(
+                        nome: _controllerNome.text,
+                        email: _controllerEmail.text,
+                        login: _controllerLogin.text,
+                        senha: _controllerSenha.text,
+                        avatar: _controllerAvatar.text,
+                      ));
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text("Salvar")
+              )
+            ],
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +180,7 @@ class _TelaLoginState extends State<TelaLogin> {
                 child: Column(
                   children: [
                     TextFormField(
-                      controller: controllerLogin,
+                      controller: _controllerLogin,
                       keyboardType: TextInputType.text,
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(),
@@ -72,7 +189,7 @@ class _TelaLoginState extends State<TelaLogin> {
                     ),
                     const SizedBox(height: 15,),
                     TextFormField(
-                      controller: controllerSenha,
+                      controller: _controllerSenha,
                       keyboardType: TextInputType.text,
                       decoration: const InputDecoration(
                           border: OutlineInputBorder(),
@@ -82,12 +199,18 @@ class _TelaLoginState extends State<TelaLogin> {
                     ),
                     ElevatedButton(
                         onPressed: (){
-                          autenticar(context, controllerLogin.text, controllerSenha.text);
+                          autenticar(context, _controllerLogin.text, _controllerSenha.text);
                         },
-                        child: const Text("Ok")
+                        child: const Text("Entrar")
                     )
                   ],
                 )
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  adicionarNovoUsuario();
+                },
+                child: const Text("Cadastrar-se")
             )
           ],
         ),
